3f99d02d3b687690e00b952f06475cb2052f789b
AJ <ajlato01@gmail.com>
2020-03-06 07:40:51 -0500

    Added Refresh

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './all.css'
 
 function randomSearch(items) {
 
-    return items[Math.floor(Math.random() * items.length)];
+    return items[Math.floor(Math.random() * items.length)]
 
 }
 
@@ -14,6 +14,7 @@ class App extends React.Component {
         super(props);
 
         this.state = {
+            key: 1,
             items: [],
             isLoaded: false,
             items2: [],
@@ -22,6 +23,32 @@ class App extends React.Component {
             search: ['work', 'job', 'corporate', 'hr', 'human resources', 'office', 'hire', 'meeting', 'board room', 'contract']
         }
 
+    }
+
+    refresh = () => {
+        fetch('https://corporatebs-generator.sameerkumar.website/')
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    items: json,
+                    isLoaded: true,
+                })
+            }).catch((err) => {
+                console.log(err);
+            });
+
+        fetch(`https://pixabay.com/api/?key=15416997-ea9683b19c946a25c1f0b4d67&q=${randomSearch(this.state.search)}&image_type=photo&pretty=true`)
+            .then(res => res.json())
+            .then(json => {
+                const rndNumb = Math.floor((Math.random() * json.hits.length))
+                this.setState({
+                    items2: json,
+                    isLoaded2: true,
+                    imgUrl: json.hits[rndNumb].largeImageURL
+                })
+            }).catch((err) => {
+                console.log(err);
+            });
     }
 
     componentDidMount() {
@@ -71,6 +98,7 @@ class App extends React.Component {
             width: '90%',
             height: '50%',
             display: 'flex',
+            flexDirection: 'column',
             justifyContent: 'center',
             alignItems: 'center',
             borderRadius: '3px',
@@ -78,13 +106,30 @@ class App extends React.Component {
             textAlign: 'center'
         }
 
+        const svg = {
+            marginTop: '20px',
+            transition: 'transform 0.5s ease',
+        }
+
         if (!isLoaded && !isLoaded2)
-            return <div>Loading...</div>;
+            return <div>Loading...</div>
 
         return (
             <div style={myStyle}>
                 <div style={myStyle2}>
                     {items.phrase}
+                    <svg
+                        class="animate rotate"
+                        style={svg}
+                        onClick={this.refresh}
+                        id="Capa_1"
+                        enable-background="new 0 0 551.13 551.13"
+                        height="10%"
+                        viewBox="0 0 551.13 551.13"
+                        width="10%"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path d="m482.239 310.011c0 113.966-92.707 206.674-206.674 206.674s-206.674-92.708-206.674-206.674c0-102.208 74.639-187.086 172.228-203.562v65.78l86.114-86.114-86.114-86.115v71.641c-116.653 16.802-206.673 117.139-206.673 238.37 0 132.955 108.164 241.119 241.119 241.119s241.119-108.164 241.119-241.119z" />
+                    </svg>
                 </div>
             </div>
         );
@@ -93,4 +138,4 @@ class App extends React.Component {
 
 }
 
-export default App;
+export default App
